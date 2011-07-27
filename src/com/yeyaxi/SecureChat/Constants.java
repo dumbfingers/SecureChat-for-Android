dcc9ea5eb0447d8c961ee0f26158b12a8bc360a6
ss1271 <ss1271@gmail.com>
2011-07-27 23:14:49 +0100

    Split JPake onto a new .java file

--- src/com/yeyaxi/SecureChat/Constants.java
+++ src/com/yeyaxi/SecureChat/Constants.java
@@ -4,5 +4,7 @@ package com.yeyaxi.SecureChat;
 public class Constants {
 	public static final int AES_KEY_SIZE = 128;
 	public static final String SMS_RECIPIENT = "5554";
+	
+	//public String PHONE_UID = "";
 
 }
